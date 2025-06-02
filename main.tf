9e38e609d04ea7269d5583bd8236fa249d6b4729
Assefa_Altaye <assekopsynwa@gmail.com>
2025-06-02 11:16:47 -0500

    create VPC and subnet

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/24"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "cwc-vpc"
+  }
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/25"
+
+  tags = {
+    Name = "cwc-public-subnet"
+  }
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.128/25"
+
+  tags = {
+    Name = "cwc-private-subnet"
+  }
+}
+
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-0f88e80871fd81e91"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
+
