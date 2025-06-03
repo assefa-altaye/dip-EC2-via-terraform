d1d46e52c7440fa0a357f0d873248207ab22979b
Assefa_Altaye <assekopsynwa@gmail.com>
2025-06-03 17:49:31 -0500

    route table association

--- main.tf
+++ main.tf
@@ -44,9 +44,22 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-output "vpc_id" {
-  value = aws_vpc.main.id
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "cwc-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 
