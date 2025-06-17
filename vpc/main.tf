79447e4565fc9cd7e92c12fa1fb7cd73ec7cbadb
Assefa_Altaye <assekopsynwa@gmail.com>
2025-06-16 20:28:51 -0500

    create private subnet module

--- vpc/main.tf
+++ vpc/main.tf
@@ -62,3 +62,38 @@ resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
+resource "aws_eip" "nat" {
+    count = 2
+  depends_on = [ aws_internet_gateway.gw ]
+}
+
+resource "aws_nat_gateway" "gw_nat" {
+    count = 2
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public_subnet[count.index].id
+
+  tags = {
+    Name = "${var.app_name}-natgw-${count.index + 1}"
+  }
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "private_route_table" {
+    count = 2
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gw_nat[count.index].id
+  }
+
+  tags = {
+    Name = "${var.app_name}-private-route-table-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+    count = 2
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table[count.index].id
+}
